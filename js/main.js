14f88ebf32dc6497bd83d1033e345296f6169b08
Restyled.io <commits@restyled.io>
2020-05-26 10:00:09 +0000

    Restyled by prettier

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const menuToggler = document.querySelector(".sidebar__toggler");
 const sidebar = document.querySelector(".sidebar");
 
-menuToggler.addEventListener("click", function() {
+menuToggler.addEventListener("click", function () {
   if (this.classList.contains("open")) {
     this.classList.remove("open");
     sidebar.classList.add("collapse");
