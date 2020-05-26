21164f58a4233b40fb6711a5a7bedb9a39b32cdf
Restyled.io <commits@restyled.io>
2020-05-26 10:00:08 +0000

    Restyled by clang-format

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const menuToggler = document.querySelector(".sidebar__toggler");
 const sidebar = document.querySelector(".sidebar");
 
-menuToggler.addEventListener("click", function () {
-    if (this.classList.contains("open")) {
-        this.classList.remove("open");
-        sidebar.classList.add("collapse");
-    } else {
-        this.classList.add("open");
-        sidebar.classList.remove("collapse");
-    }
+menuToggler.addEventListener("click", function() {
+  if (this.classList.contains("open")) {
+    this.classList.remove("open");
+    sidebar.classList.add("collapse");
+  } else {
+    this.classList.add("open");
+    sidebar.classList.remove("collapse");
+  }
 });
